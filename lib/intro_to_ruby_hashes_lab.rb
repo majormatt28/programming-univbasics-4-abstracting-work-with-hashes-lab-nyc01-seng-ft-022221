46c35d825614dc1c36f41ce46cac43929b3ae1bd
Mathew E Newton <elijah2895@hotmail.com>
2021-02-06 17:51:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  key.to_i = value[:key]
+  key = value[:key]
 end
 
 def read_from_hash(hash, key)
